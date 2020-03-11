d0c3e62f4e0175b2f83be0e97ea675ca8b8ba065
subhra74 <subhradasgupta64@gmaii.com>
2020-03-11 21:21:34 +0100

    updated extension

--- extensions/xdm-browser-monitor/chrome/popup.js
+++ extensions/xdm-browser-monitor/chrome/popup.js
@@ -42,6 +42,8 @@ function renderList(arr) {
 
     var table = document.getElementById("table");
 
+    console.log("total element: "+arr.length);
+
     for (var i = 0; i < arr.length; i++) {
         var listItem = arr[i];
 
@@ -53,20 +55,42 @@ function renderList(arr) {
         var row = table.insertRow(0);
         var cell = row.insertCell(0);
 
-        if (i < arr.length - 1) {
-            var hr = document.createElement('hr');
-            cell.appendChild(hr);
-        }
+        // if (i < arr.length - 1) {
+        //     var hr = document.createElement('hr');
+        //     cell.appendChild(hr);
+        // }
 
-        var p = document.createElement('p');
-        var node = document.createTextNode("[" + info + "] " + text);
-        p.appendChild(node);
+        //cell.setAttribute("style","padding: 10px;");
 
-        cell.appendChild(p);
+        var border = "";// "border-bottom: 1px solid rgb(240,240,240);";
+        // if (i == arr.length - 1) {
+        //     console.log("last element: "+i);
+        //     border = "";
+        // }
 
-        p.id = listItem.id;
+        var div = document.createElement('div');
+        div.setAttribute("style", "padding: 10px; display: flex; flex-direction: column;" + border);
 
-        p.addEventListener('click', function (e) {
+        var p1 = document.createElement('span');
+        p1.id=listItem.id;
+        p1.setAttribute("style", "font-family:helvetica,arial,courier; font-size: 14px; cursor: pointer;");
+        var node = document.createTextNode(text);
+        p1.appendChild(node);
+
+        var p2 = document.createElement('span');
+        p2.setAttribute("style", "font-family:helvetica,arial,courier; font-size: 12px;");
+        node = document.createTextNode(info);
+        p2.appendChild(node);
+
+        div.appendChild(p1);
+        div.appendChild(p2);
+
+        cell.appendChild(div);
+
+        //div.id = listItem.id;
+
+        div.addEventListener('click', function (e) {
+            //alert("Sending message for download - id: "+e.target.id+ "target: "+e.target);
             chrome.runtime.sendMessage({ type: "vid", itemId: e.target.id });
             window.close();
         });
