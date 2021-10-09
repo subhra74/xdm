39abee6fc6445aaf5f36cd5c2575c4d25c1e9d31
Jhon Fernandez <jhondev.code@gmail.com>
2021-10-08 23:52:31 -0500

    YoutubeDLHandler code was refactored

--- app/src/main/java/xdman/videoparser/YoutubeDLHandler.java
+++ app/src/main/java/xdman/videoparser/YoutubeDLHandler.java
@@ -1,5 +1,13 @@
 package xdman.videoparser;
 
+import org.tinylog.Logger;
+import xdman.Config;
+import xdman.network.ProxyResolver;
+import xdman.network.http.WebProxy;
+import xdman.util.IOUtils;
+import xdman.util.StringUtils;
+import xdman.util.XDMUtils;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -7,155 +15,142 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import xdman.Config;
-import xdman.network.ProxyResolver;
-import xdman.network.http.WebProxy;
-import xdman.util.StringUtils;
-import xdman.util.XDMUtils;
-
-import org.tinylog.Logger;
-
 public class YoutubeDLHandler {
-	private Process proc;
-	private int exitCode;
-	private String url;
-	private String ydlLocation;
-	private boolean stop;
-	private String user, pass;
+    private Process proc;
+    private int exitCode;
+    private final String url;
+    private final String ydlLocation;
+    private boolean stop;
+    private final String user;
+	private final String pass;
 
-	private ArrayList<YdlResponse.YdlVideo> videos;
+    private final ArrayList<YdlResponse.YdlVideo> videos;
 
-	public YoutubeDLHandler(String url, String user, String pass) {
-		this.url = url;
-		this.videos = new ArrayList<>();
-		File ydlFile = new File(Config.getInstance().getDataFolder(),
-				System.getProperty("os.name").toLowerCase().contains("windows") ? "youtube-dl.exe" : "youtube-dl");
-		if (!ydlFile.exists()) {
-			ydlFile = new File(XDMUtils.getJarFile().getParentFile(),
-					System.getProperty("os.name").toLowerCase().contains("windows") ? "youtube-dl.exe" : "youtube-dl");
-		}
-		ydlLocation = ydlFile.getAbsolutePath();
-		this.user = user;
-		this.pass = pass;
-	}
+    public YoutubeDLHandler(String url, String user, String pass) {
+        this.url = url;
+        this.videos = new ArrayList<>();
+        File ydlFile = new File(Config.getInstance().getDataFolder(),
+                System.getProperty("os.name").toLowerCase().contains("windows") ? "youtube-dl.exe" : "youtube-dl");
+        if (!ydlFile.exists()) {
+            ydlFile = new File(XDMUtils.getJarFile().getParentFile(),
+                    System.getProperty("os.name").toLowerCase().contains("windows") ? "youtube-dl.exe" : "youtube-dl");
+        }
+		this.ydlLocation = ydlFile.getAbsolutePath();
+        this.user = user;
+        this.pass = pass;
+    }
 
-	public void start() {
-		File tmpError = new File(Config.getInstance().getTemporaryFolder(), UUID.randomUUID().toString());
-		File tmpOutput = new File(Config.getInstance().getTemporaryFolder(), UUID.randomUUID().toString());
-		InputStream in = null;
-		try {
-			List<String> args = new ArrayList<String>();
-			args.add(ydlLocation);
-			args.add("--no-warnings");
-			args.add("-q");
-			args.add("-i");
-			args.add("-J");
-			if (!(StringUtils.isNullOrEmptyOrBlank(user) || StringUtils.isNullOrEmptyOrBlank(pass))) {
-				args.add("-u");
-				args.add(user);
-				args.add("-p");
-				args.add(pass);
+    public void start() {
+        File tmpError = new File(Config.getInstance().getTemporaryFolder(), UUID.randomUUID().toString());
+        File tmpOutput = new File(Config.getInstance().getTemporaryFolder(), UUID.randomUUID().toString());
+        InputStream in = null;
+        try {
+            List<String> args = new ArrayList<>();
+            args.add(this.ydlLocation);
+            args.add("--no-warnings");
+            args.add("-q");
+            args.add("-i");
+            args.add("-J");
+            if (!(StringUtils.isNullOrEmptyOrBlank(this.user) || StringUtils.isNullOrEmptyOrBlank(this.pass))) {
+                args.add("-u");
+                args.add(this.user);
+                args.add("-p");
+                args.add(this.pass);
+            }
+
+            WebProxy webproxy = ProxyResolver.resolve(url);
+            if (webproxy != null) {
+                StringBuilder sb = new StringBuilder();
+                String user = Config.getInstance().getProxyUser();
+                String pass = Config.getInstance().getProxyPass();
+                if (!(StringUtils.isNullOrEmptyOrBlank(user) || StringUtils.isNullOrEmptyOrBlank(pass))) {
+                    sb.append(user).append(":").append(pass);
+                }
+                String proxy = "http://" + webproxy.getHost();
+                int port = webproxy.getPort();
+                if (port > 0 && port != 80) {
+                    sb.append(":").append(port);
+                }
+                if (sb.length() > 0) {
+                    sb.append("@");
+                }
+                sb.append(proxy);
+                args.add("--proxy");
+                args.add(sb.toString());
+            }
+
+            // args.add("--proxy");
+            // args.add("http://127.0.0.1:8888");
+            args.add(url);
+
+            ProcessBuilder pb = new ProcessBuilder(args);
+			for (String arg : args) {
+				Logger.info(arg);
 			}
 
-			WebProxy webproxy = ProxyResolver.resolve(url);
-			if (webproxy != null) {
-				StringBuilder sb = new StringBuilder();
-				String user = Config.getInstance().getProxyUser();
-				String pass = Config.getInstance().getProxyPass();
-				if (!(StringUtils.isNullOrEmptyOrBlank(user) || StringUtils.isNullOrEmptyOrBlank(pass))) {
-					sb.append(user + ":" + pass);
-				}
-				String proxy = "http://" + webproxy.getHost();
-				int port = webproxy.getPort();
-				if (port > 0 && port != 80) {
-					sb.append(":" + port);
-				}
-				if (sb.length() > 0) {
-					sb.append("@");
-				}
-				sb.append(proxy);
-				args.add("--proxy");
-				args.add(sb.toString());
-			}
+            Logger.info("Writing JSON to: " + tmpOutput);
 
-			// args.add("--proxy");
-			// args.add("http://127.0.0.1:8888");
-			args.add(url);
+            pb.redirectError(tmpError);
+            pb.redirectOutput(tmpOutput);
+            proc = pb.start();
 
-			ProcessBuilder pb = new ProcessBuilder(args);
-			for (int i = 0; i < args.size(); i++) {
-				Logger.info(args.get(i));
-			}
+            // InputStream in = proc.getInputStream();
+            // byte[] buf = new byte[8192];
+            // while (true) {
+            // int x = in.read(buf);
+            // if (x == -1)
+            // break;
+            // bout.write(buf, 0, x);
+            // }
 
-			Logger.info("Writing JSON to: " + tmpOutput);
+            // OutputStream out = new FileOutputStream("test.txt");
+            // out.write(bout.toByteArray());
+            // out.close();
 
-			pb.redirectError(tmpError);
-			pb.redirectOutput(tmpOutput);
-			proc = pb.start();
+            // BufferedReader br=new BufferedReader(new InputStreamReader(new
+            // FileInputStream(tmpOutput)));
+            // StringBuilder json=new StringBuilder();
+            // while(true) {
+            // String ln=br.readLine();
+            // if(ln==null)break;
+            // json.append(ln+"\n");
+            // }
+            // br.close();
+            // //String json = new String(bout.toByteArray());
+            // System.out.println("----json: " + json);
+            // System.out.println("----json end ----");
+			this.exitCode = this.proc.waitFor();
+            if (!this.stop) {
+                in = new FileInputStream(tmpOutput);
+				this.videos.addAll(YdlResponse.parse(in));
+                Logger.info("video found: " + this.videos.size());
+            }
+        } catch (Exception e) {
+            Logger.error(e);
+        } finally {
+			IOUtils.closeFlow(in);
+            tmpError.delete();
+            tmpOutput.delete();
+        }
+    }
 
-			// InputStream in = proc.getInputStream();
-			// byte[] buf = new byte[8192];
-			// while (true) {
-			// int x = in.read(buf);
-			// if (x == -1)
-			// break;
-			// bout.write(buf, 0, x);
-			// }
+    public int getExitCode() {
+        return this.exitCode;
+    }
 
-			// OutputStream out = new FileOutputStream("test.txt");
-			// out.write(bout.toByteArray());
-			// out.close();
+    public void setExitCode(int exitCode) {
+        this.exitCode = exitCode;
+    }
 
-			// BufferedReader br=new BufferedReader(new InputStreamReader(new
-			// FileInputStream(tmpOutput)));
-			// StringBuilder json=new StringBuilder();
-			// while(true) {
-			// String ln=br.readLine();
-			// if(ln==null)break;
-			// json.append(ln+"\n");
-			// }
-			// br.close();
-			// //String json = new String(bout.toByteArray());
-			// System.out.println("----json: " + json);
-			// System.out.println("----json end ----");
-			exitCode = proc.waitFor();
-			if (!stop) {
-				in = new FileInputStream(tmpOutput);
-				videos.addAll(YdlResponse.parse(in));
-				Logger.info("video found: " + videos.size());
-			}
-		} catch (Exception e) {
-			Logger.error(e);
-		} finally {
-			try {
-				if (in != null) {
-					in.close();
-				}
-			} catch (Exception e) {
-				Logger.error(e);
-			}
-			tmpError.delete();
-			tmpOutput.delete();
-		}
-	}
+    public ArrayList<YdlResponse.YdlVideo> getVideos() {
+        return this.videos;
+    }
 
-	public int getExitCode() {
-		return exitCode;
-	}
-
-	public void setExitCode(int exitCode) {
-		this.exitCode = exitCode;
-	}
-
-	public ArrayList<YdlResponse.YdlVideo> getVideos() {
-		return videos;
-	}
-
-	public void stop() {
-		try {
-			proc.destroy();
-		} catch (Exception e) {
-			Logger.error(e);
-		}
-	}
+    public void stop() {
+        try {
+			this.proc.destroy();
+        } catch (Exception e) {
+            Logger.error(e);
+        }
+    }
 }
