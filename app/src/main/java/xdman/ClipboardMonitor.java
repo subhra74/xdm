ade532a724c23c1aad23a03992133f7be60f39f6
Jhon Fernandez <jhondev.code@gmail.com>
2021-10-08 23:22:46 -0500

    I updated the Clipboard Monitor code

--- app/src/main/java/xdman/ClipboardMonitor.java
+++ app/src/main/java/xdman/ClipboardMonitor.java
@@ -1,96 +1,93 @@
 package xdman;
 
-import java.net.URL;
-
+import org.tinylog.Logger;
 import xdman.downloaders.metadata.HttpMetadata;
 import xdman.util.StringUtils;
 import xdman.util.XDMUtils;
 
-import org.tinylog.Logger;
+import java.net.URL;
 
 public class ClipboardMonitor implements Runnable {
 
-	private String lastContent;
-	private static ClipboardMonitor _this;
-	private Thread t;
+    private static ClipboardMonitor _this;
+    private String lastContent;
+    private Thread t;
 
-	public static ClipboardMonitor getInstance() {
-		if (_this == null) {
-			_this = new ClipboardMonitor();
-		}
-		return _this;
-	}
+    private ClipboardMonitor() {
+    }
 
-	private ClipboardMonitor() {
+    public static ClipboardMonitor getInstance() {
+        if (_this == null) {
+            _this = new ClipboardMonitor();
+        }
+        return _this;
+    }
 
-	}
+    public void startMonitoring() {
+        try {
+            if (this.t == null) {
+                this.t = new Thread(this);
+                this.t.start();
+            }
+        } catch (Exception e) {
+            Logger.error(e);
+        }
 
-	public void startMonitoring() {
-		try {
-			if (t == null) {
-				t = new Thread(this);
-				t.start();
-			}
-		} catch (Exception e) {
-			Logger.error(e);
-		}
+    }
 
-	}
+    public void stopMonitoring() {
+        try {
+            if (this.t != null && this.t.isAlive()) {
+                this.t.interrupt();
+                this.t = null;
+            }
+        } catch (Exception e) {
+            Logger.error(e);
+        }
 
-	public void stopMonitoring() {
-		try {
-			if (t != null && t.isAlive()) {
-				t.interrupt();
-				t = null;
-			}
-		} catch (Exception e) {
-			Logger.error(e);
-		}
+    }
 
-	}
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                String txt = XDMUtils.getClipBoardText();
+                if (StringUtils.isNullOrEmptyOrBlank(txt)) {
+                    return;
+                }
+                if (!txt.equals(this.lastContent)) {
+                    Logger.info("New content: " + txt);
+                    this.lastContent = txt;
+                    try {
+                        new URL(txt);
+                        HttpMetadata md = new HttpMetadata();
+                        md.setUrl(txt);
+                        String file = XDMUtils.getFileName(txt);
+                        String ext = XDMUtils.getExtension(file);
+                        if (!StringUtils.isNullOrEmptyOrBlank(ext)) {
+                            ext = ext.toUpperCase().replace(".", "");
+                        }
 
-	@Override
-	public void run() {
-		try {
-			while (true) {
-				String txt = XDMUtils.getClipBoardText();
-				if (StringUtils.isNullOrEmptyOrBlank(txt)) {
-					return;
-				}
-				if (!txt.equals(lastContent)) {
-					Logger.info("New content: " + txt);
-					lastContent = txt;
-					try {
-						new URL(txt);
-						HttpMetadata md = new HttpMetadata();
-						md.setUrl(txt);
-						String file = XDMUtils.getFileName(txt);
-						String ext = XDMUtils.getExtension(file);
-						if (!StringUtils.isNullOrEmptyOrBlank(ext)) {
-							ext = ext.toUpperCase().replace(".", "");
-						}
-
-						String[] arr = Config.getInstance().getFileExts();
-						boolean found = false;
-						for (int i = 0; i < arr.length; i++) {
-							if (arr[i].contains(ext)) {
-								found = true;
-								break;
-							}
-						}
-						if (found) {
-							XDMApp.getInstance().addDownload(md, file);
-						}
-					} catch (Exception e) {
-						Logger.error(e);
-					}
-
-				}
-				Thread.sleep(1000);
-			}
-		} catch (Exception e) {
-			Logger.error(e);
-		}
-	}
+                        String[] arr = Config.getInstance().getFileExts();
+                        boolean found = false;
+                        for (String s : arr) {
+                            if (s.contains(ext)) {
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found) {
+                            XDMApp.getInstance().addDownload(md, file);
+                        }
+                    } catch (Exception e) {
+                        Logger.error(e);
+                    }
+                }
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            Logger.error(e);
+        }
+    }
 
 }
