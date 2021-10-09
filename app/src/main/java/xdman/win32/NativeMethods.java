a14fb652235d121ea2307b64a0eb8516f10f8d43
Jhon Fernandez <jhondev.code@gmail.com>
2021-10-08 23:41:53 -0500

    The NativeMethods code has been updated

--- app/src/main/java/xdman/win32/NativeMethods.java
+++ app/src/main/java/xdman/win32/NativeMethods.java
@@ -1,39 +1,38 @@
 package xdman.win32;
 
-import java.io.File;
-
+import org.tinylog.Logger;
 import xdman.util.XDMUtils;
 
-import org.tinylog.Logger;
+import java.io.File;
 
 public class NativeMethods {
-	private static NativeMethods _me;
+    private static NativeMethods _me;
 
-	public static NativeMethods getInstance() {
-		if (_me == null) {
-			_me = new NativeMethods();
-		}
-		return _me;
-	}
+    private NativeMethods() {
+        String dllPath = new File(XDMUtils.getJarFile().getParentFile(), "xdm_native.dll").getAbsolutePath();
+        try {
+            System.load(dllPath);
+        } catch (Exception e) {
+            Logger.error(e);
+        }
+    }
 
-	private NativeMethods() {
-		String dllPath = new File(XDMUtils.getJarFile().getParentFile(), "xdm_native.dll").getAbsolutePath();
-		try {
-			System.load(dllPath);
-		} catch (Exception e) {
-			Logger.error(e);
-		}
-	}
+    public static NativeMethods getInstance() {
+        if (_me == null) {
+            _me = new NativeMethods();
+        }
+        return _me;
+    }
 
-	public final native void keepAwakePing();
+    public final native void keepAwakePing();
 
-	public final native void addToStartup(String key, String value);
+    public final native void addToStartup(String key, String value);
 
-	public final native boolean presentInStartup(String key, String value);
+    public final native boolean presentInStartup(String key, String value);
 
-	public final native void removeFromStartup(String key);
-	
-	public final native String getDownloadsFolder();
-	
-	public final native String stringTest(String str);
+    public final native void removeFromStartup(String key);
+
+    public final native String getDownloadsFolder();
+
+    public final native String stringTest(String str);
 }
