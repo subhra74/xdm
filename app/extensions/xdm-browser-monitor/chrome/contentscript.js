05f126c05162b5e825c327827e9da1589e208e45
jhondevcode <jhondev.code@gmail.com>
2021-10-29 22:11:49 -0500

    The contentscript.js file was updated making it more concise

--- app/extensions/xdm-browser-monitor/chrome/contentscript.js
+++ app/extensions/xdm-browser-monitor/chrome/contentscript.js
@@ -1,6 +1,4 @@
 console.log("hello from content script");
-var arr = [], l = document.links;
-for(var i=0; i<l.length; i++) {
-  arr.push(l[i].href);
-}
-chrome.runtime.sendMessage({type: "links",links: arr});
+let links = document.links;
+let array = links.map(link => link.href);
+chrome.runtime.sendMessage({ type: "links", links: arr });
