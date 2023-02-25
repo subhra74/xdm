77db1dc8b26ef09b68d64f412445267a57e4b13e
subhra74 <subhradasgupta64@gmaii.com>
2023-02-25 13:19:30 +0530

    removed tab filtering

--- app/XDM/chrome-extension/app.js
+++ app/XDM/chrome-extension/app.js
@@ -156,19 +156,25 @@ export default class App {
         chrome.action.setIcon({ path: this.getActionIcon() });
         let vc = "";
         if (this.videoList && this.videoList.length > 0) {
-            let len = this.videoList.filter(vid => {
-                if (!vid.tabId) {
-                    return true;
-                }
-                if (vid.tabId == '-1') {
-                    return true;
-                }
-                return (vid.tabId == this.activeTabId);
-            }).length;
+            let len = this.videoList.length;
             if (len > 0) {
                 vc = len + "";
             }
         }
+        // if (this.videoList && this.videoList.length > 0) {
+        //     let len = this.videoList.filter(vid => {
+        //         if (!vid.tabId) {
+        //             return true;
+        //         }
+        //         if (vid.tabId == '-1') {
+        //             return true;
+        //         }
+        //         return (vid.tabId == this.activeTabId);
+        //     }).length;
+        //     if (len > 0) {
+        //         vc = len + "";
+        //     }
+        // }
         chrome.action.setBadgeText({ text: vc });
         if (!this.connector.isConnected()) {
             this.logger.log("Not connected...");
@@ -243,12 +249,13 @@ export default class App {
         if (request.type === "stat") {
             let resp = {
                 enabled: this.isMonitoringEnabled(),
-                list: this.videoList.filter(vid => {
-                    if (!vid.tabId) {
-                        return true;
-                    }
-                    return (vid.tabId == this.activeTabId);
-                })
+                list: this.videoList
+                // list: this.videoList.filter(vid => {
+                //     if (!vid.tabId) {
+                //         return true;
+                //     }
+                //     return (vid.tabId == this.activeTabId);
+                // })
             };
             sendResponse(resp);
         }
